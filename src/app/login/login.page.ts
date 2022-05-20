cda1346e6949ebc08bfba79695fbbe55ce72e774
JoaoVitorC4 <jvcecato@hotmail.com>
2022-05-19 21:20:15 -0400

    criando tela de login

--- src/app/login/login.page.ts
+++ src/app/login/login.page.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-login',
@@ -6,10 +7,20 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./login.page.scss'],
 })
 export class LoginPage implements OnInit {
+  public login: FormGroup;
 
-  constructor() { }
+  constructor(
+    private formBuilder: FormBuilder,
+  ) { }
 
   ngOnInit() {
+    this.setarFormulario();
   }
 
+  private setarFormulario() {
+    this.login = this.formBuilder.group({
+      usuario: [ , Validators.required],
+      senha: [ , Validators.required],
+    });
+  }
 }
